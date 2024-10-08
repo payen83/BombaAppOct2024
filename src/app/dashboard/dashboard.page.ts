6034d9b0a0a610fc8e1931c07bfab5efcfaf4d0b
Ferdy Fauzi <payen83@gmail.com>
2024-10-08 17:57:20 +0800

    update

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -5,6 +5,11 @@ interface Bulletin {
   imageURL: string
 }
 
+interface Menu {
+  title: string,
+  imageURL: string
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
@@ -16,6 +21,12 @@ export class DashboardPage implements OnInit {
     { title: "Bantuan Banjir", imageURL: 'assets/images/bulletin-2.jpeg' },
     { title: "Hari Ibu Bapa", imageURL: 'assets/images/bulletin-1.jpeg' },
     { title: "Hari Bomba Sedunia", imageURL:'assets/images/bulletin-default.png' }
+  ];
+
+  public listMenu: Array<Menu> = [
+    { title: "Senarai", imageURL: 'assets/menu/Buletin.png' },
+    { title: "Carian", imageURL: 'assets/menu/Carian.png' },
+    { title: "Dokumen", imageURL:'assets/menu/Dokumen.png' }
   ];
 
   constructor() { }
