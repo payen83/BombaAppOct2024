81515e7f3e19b0979149efda1f68702af67e5f45
Ferdy Fauzi <payen83@gmail.com>
2024-10-08 16:23:12 +0800

    update bulletin

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Bulletin {
+  title: string,
+  imageURL: string
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+
+  public listBulletin: Array<Bulletin> = [
+    { title: "Bantuan Banjir", imageURL: 'assets/images/bulletin-2.jpeg' },
+    { title: "Hari Ibu Bapa", imageURL: 'assets/images/bulletin-1.jpeg' },
+    { title: "Hari Bomba Sedunia", imageURL:'assets/images/bulletin-default.png' }
+  ];
 
   constructor() { }
 
