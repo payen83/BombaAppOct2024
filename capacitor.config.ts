9ed51e1b58d9e0a1dde16379b4063fd1a2e21e6f
Ferdy Fauzi <payen83@gmail.com>
2024-10-10 17:00:43 +0800

    update android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'my.gov.jbpm.app',
   appName: 'BombaApp',
   webDir: 'www'
 };
--- src/app/tab2/modal/modal.component.ts
+++ src/app/tab2/modal/modal.component.ts
@@ -1,14 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { ModalController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent  implements OnInit {
+export class ModalComponent implements AfterViewInit {
+  private listener: any;
+  constructor(private modalCtrl: ModalController) { }
 
-  constructor() { }
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.startScan().then((result: any)=>{
+        this.modalCtrl.dismiss(result);
+      });
+    }, 250)
+  }
 
-  ngOnInit() {}
+  startScan(){
+    return new Promise(async (resolve) => {
+      document.querySelector('body')?.classList.add('barcode-scanner-active');
+      this.listener = await BarcodeScanner.addListener('barcodesScanned', result => {
+        this.stopScan();
+        resolve(result);
+      })
+      await BarcodeScanner.startScan();
+    })
+  }
+
+  async stopScan(){
+    await this.listener.remove();
+    document.querySelector('body')?.classList.remove('barcode-scanner-active');
+    return await BarcodeScanner.stopScan();
+  }
+
+  closeModal(){
+    this.stopScan();
+    this.modalCtrl.dismiss();
+  }
 
 }
