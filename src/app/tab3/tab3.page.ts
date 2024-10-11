7bf0987bcacfe8deac5c86b0646c5e4408f06115
Ferdy Fauzi <payen83@gmail.com>
2024-10-11 11:59:17 +0800

    update map

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+declare var L: any;
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
+  map: any;
   constructor() {}
+
+  ionViewWillEnter(){
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(this.map);
+  }
 
 }
